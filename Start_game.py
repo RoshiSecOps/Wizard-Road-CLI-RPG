89b5c44233ecb8826e2cf6250fb5e565b8b3aecb
Ivan Popov <116061712+RoshiSecOps@users.noreply.github.com>
2026-01-12 03:21:48 -0800

    1.16: Once the fight is over, now you will be returned to main menu, to add a new enemy, if first is dead.

--- Start_game.py
+++ Start_game.py
@@ -6,18 +6,19 @@ player_one = create_character()
 first_enemy = Orc("Rengar", 150, 4)
 
 def trial_gameplay():
-    open_main_menu()
-    player_choice = int(input("You choose?(1-4): "))
-    if player_choice == 1:
-        full_battle(first_enemy, player_one)
-    elif player_choice == 2:
-        return "Work in progress"
-    elif player_choice == 3:
-        return player_one.get_all_stats()
-    elif player_choice == 4:
-        return "Exiting"
-    else:
-        return "Invalid Choice"
+    while player_one.is_alive() == True:
+        open_main_menu()
+        player_choice = int(input("You choose?(1-4): "))
+        if player_choice == 1:
+            full_battle(first_enemy, player_one)
+        elif player_choice == 2:
+            return "Work in progress"
+        elif player_choice == 3:
+            return player_one.get_all_stats()
+        elif player_choice == 4:
+            return "Exiting"
+        else:
+            return "Invalid Choice"
 
 play_a_game = trial_gameplay()
 print(play_a_game)
